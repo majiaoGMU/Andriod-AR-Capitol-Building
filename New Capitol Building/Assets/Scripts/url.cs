75120720521120fa815d23d73428c5f6bb592400
Jiao Ma <majiao.summer@gmail.com>
2017-08-08 11:43:15 -0400

    The whole AR History Capitol Building backup

---
+++ New Capitol Building/Assets/Scripts/url.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class url : MonoBehaviour {
+
+    public void urlLinkOrWeb1 ()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/United_States_Capitol");
+    }
+    public void urlLinkOrWeb2()
+    {
+        Application.OpenURL("https://www.tripadvisor.com/Attraction_Review-g28970-d143718-Reviews-U_S_Capitol-Washington_DC_District_of_Columbia.html");
+    }
+}
